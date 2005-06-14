b19d35cdb52d18dcd235e336dec471e9c68cb44f
de <de@512c2683-75f9-0310-acad-d911283a7351>
2005-06-14 21:35:20 +0000

    tool views
    git-svn-id: svn://svn.valgrind.org/valkyrie/trunk@10 512c2683-75f9-0310-acad-d911283a7351

---
+++ src/tool_views/massif_view.h
@@ -0,0 +1,31 @@
+/* ---------------------------------------------------------------------
+ * definition of MassifView                                massif_view.h
+ * ---------------------------------------------------------------------
+ */
+
+#ifndef __VK_VIEW_MASSIF_H
+#define __VK_VIEW_MASSIF_H
+
+#include "tool_view.h"
+
+class MassifView : public ToolView
+{
+	Q_OBJECT
+public:
+  MassifView( QWidget* parent, VkObject* obj );
+  ~MassifView();
+
+	bool run() { return true; }
+	void stop() { }
+	void clear() { }
+
+public slots:
+		void processExited() { }
+
+protected:
+	//virtual void procFinished() { }
+};
+
+
+
+#endif
