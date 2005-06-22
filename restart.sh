4852c6d3c9fb6433afa97bc36bafd06b1c7670c7
de <de@512c2683-75f9-0310-acad-d911283a7351>
2005-06-22 17:52:20 +0000

    blah
    git-svn-id: svn://svn.valgrind.org/valkyrie/trunk@20 512c2683-75f9-0310-acad-d911283a7351

--- restart.sh
+++ restart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#bakClean -r
+#rm ./bin/valkyrie
+#make clean
+#qmake -o Makefile valkyrie.pro
+#make && ./bin/valkyrie
 bakClean -r
-rm ./bin/valkyrie
+rm -f ./bin/valkyrie
 make clean
-qmake -o Makefile valkyrie.pro
+./configure --thread=yes --debug --vg-exec=/home/sewardj/Vg3LINE/trunk/Inst/bin/valgrind --vg-supp=/home/sewardj/Vg3LINE/trunk/Inst/lib/valgrind/
 make && ./bin/valkyrie
