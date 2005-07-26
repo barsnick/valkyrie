b10ebcddc32a631630680fbf731bb5f7ade44a11
de <de@512c2683-75f9-0310-acad-d911283a7351>
2005-07-26 13:46:36 +0000

    Added code for new xml tag 'xml-user-comment' to get written to file
    git-svn-id: svn://svn.valgrind.org/valkyrie/trunk@137 512c2683-75f9-0310-acad-d911283a7351

--- src/tool_utils/logfile.cpp
+++ src/tool_utils/logfile.cpp
@@ -111,10 +111,12 @@ bool LogFile::save( QString fname )
     stream << "  <line>" << preamble->lines[i] << "</line>\n";
   stream << "</preamble>\n\n";
 
-  /* pid, ppid, tool */
+  /* pid, ppid, tool, usercomment */
   stream << "<pid>"  << info->pid  << "</pid>\n";
   stream << "<ppid>" << info->ppid << "</ppid>\n";
   stream << "<tool>" << info->tool << "</tool>\n\n";
+  if ( !info->userComment.isEmpty() )
+    stream << "<usercomment>" << info->userComment << "<usercomment>\n";
 
   /* args stuff */
   stream << "<args>\n";
